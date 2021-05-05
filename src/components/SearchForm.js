069453ea2b178a96baac03cbe1cd281ecf61436c
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-05-04 23:37:38 -0500

    added SearchForm.js file

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,14 +4,13 @@ function SearchForm(props) {
   return (
     <form>
       <div className="form-group">
-        <label htmlFor="search">Search:</label>
         <input
           onChange={props.handleInputChange}
           value={props.search}
           name="search"
           type="text"
           className="form-control"
-          placeholder="Search for a Gif"
+          placeholder="Search"
           id="search"
         />
         <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
